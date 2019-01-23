649816c11d5a558df017617156ecd0b308fafa0c
majid mohamadi <m.shutdown57@gmail.com>
2019-01-24 00:27:23 +0330

    Added
    get all posts method
    get post by id method
    add post method
    delete post by id method

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
+
+import { Posts } from '../mocks/post.mock';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+    posts = Posts;
+
+    getPosts(): Promise<any> {
+        return new Promise(resolver => {
+            resolve(this.posts);
+        });
+    }
+
+    getPost(postID): Promise<any> {
+        let id = Number(postID);
+        return new Promise(resolver => {
+            const post = this.posts.find(post => post.id === id);
+            if (!post) {
+                throw new HttpException('Post does not exists!', 404);
+            }
+            resolve(post);
+        });
+    }
+
+    addPost(post): Promise<any> {
+        return new Promise(resolver => {
+            this.posts.push(post);
+            resolve(this.posts);
+        });
+    }
+
+    deletePost(postID): Promise<any> {
+        let id = Number(postID);
+        return new Promise(resolver => {
+            let index = this.posts.findIndex(post => post.id === id);
+            if (index === -1) {
+                throw new HttpException('Post does not exists!', 404);
+            }
+            this.posts.splice(1, index);
+            resolve(this.posts);
+        });
+    }
+}
