d87be35ed875da5c006ae7f7e518e7888cff76b1
majid mohamadi <m.shutdown57@gmail.com>
2019-01-24 03:36:47 +0330

    Added methods
    get all posts
    get post by id
    create post
    delete post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Query, Delete } from '@nestjs/common';
+
+import { PostService } from './post.service';
+import { CreatePostDTO } from './dto/create-post.dto';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+    constructor(private readonly postService: PostService) {}
+
+    @Get()
+    async getPosts() {
+        const posts = await this.postService.getPosts();
+        return posts;
+    }
+
+    @Get(':postID')
+    async getPost(@Param('postID') postID) {
+        const post = await this.postService.getPost(postID);
+        return post;
+    }
+
+    @Post()
+    async addPost(@Body() createPostDTO: CreatePostDTO) {
+        const post = await this.postService.addPost(createPostDTO);
+        return post;
+    }
+
+    @Delete()
+    async deletePost(@Query() query) {
+        const posts = await this.postService.deletePost(query.postID);
+        return posts;
+    }
+}
