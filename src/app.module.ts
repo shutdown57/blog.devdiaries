e5e420b5a63b806cb8d2d74e176bf689cca51dad
majid mohamadi <m.shutdown57@gmail.com>
2019-01-25 04:20:52 +0330

    Added typeorm and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
+import { Connection } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
+// import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [PostModule, TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
