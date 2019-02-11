9d027a1e875368e07959dc2fb600cbcc46bb6b90
majid mohamadi <m.shutdown57@gmail.com>
2019-02-11 20:52:24 +0330

    Added logging interceptor

---
+++ src/shared/logging.interceptor.ts
@@ -0,0 +1,15 @@
+import { Injectable, NestInterceptor, ExecutionContext, Logger } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, call$: Observable<any>): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const method = request.method;
+    const url = request.url;
+    const now = Date.now();
+
+    return call$.pipe(tap(() => Logger.log(`${method} ${url} ${Date.now() - now}ms`, context.getClass().name)));
+  }
+}
