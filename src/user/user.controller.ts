b997391b715ab8dd491c5054169c5407304ba5d7
majid mohamadi <m.shutdown57@gmail.com>
2019-02-11 21:03:10 +0330

    Added login, register and show methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { ValidationPipe } from './../shared/validation.pip';
+import { UserDTO } from './dto/user.dto';
+import { UserService } from './user.service';
+import { Controller, Post, Get, Body, Param, UsePipes } from '@nestjs/common';
 
-@Controller('user')
-export class UserController {}
+@Controller()
+export class UserController {
+
+  constructor(private userService: UserService) {}
+  @Get('api/user')
+  async show(@Param() id: string) {
+    return await this.userService.show(id);
+  }
+
+  @Post('login')
+  @UsePipes(new ValidationPipe())
+  async login(@Body() data: UserDTO) {
+    return await this.userService.login(data);
+  }
+
+  @Post('register')
+  @UsePipes(new ValidationPipe())
+  async register(@Body() data: UserDTO) {
+    return await this.userService.register(data);
+  }
+}
