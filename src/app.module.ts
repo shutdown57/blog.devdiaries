63ca89b8dab47d9d43c2ce5b785c06731f20fa00
majid mohamadi <m.shutdown57@gmail.com>
2019-02-11 20:54:48 +0330

    Added
    http error filter
    logging interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
+import { LoggingInterceptor } from './shared/logging.interceptor';
+import { HttpErrorFilter } from './shared/http-error.filter';
 import { Module } from '@nestjs/common';
 import { Connection } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostModule } from './post/post.module';
+// import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
-
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), PostModule, UserModule],
+  imports: [TypeOrmModule.forRoot(), UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {provide: APP_FILTER, useClass: HttpErrorFilter}, {provide: APP_INTERCEPTOR, useClass: LoggingInterceptor}],
 })
 export class AppModule {
   constructor(private readonly connection: Connection) {}
