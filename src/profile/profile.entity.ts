440b5f2b6227987d2655abf6f54427f2cbddd409
majid mohamadi <m.shutdown57@gmail.com>
2019-02-11 21:03:57 +0330

    Added profile module

---
+++ src/profile/profile.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity, OneToOne, Column,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn, CreateDateColumn,
+} from 'typeorm';
+
+import { User } from '../user/user.entity';
+
+@Entity('profile')
+export class Profile {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({length: 30})
+  name: string;
+
+  @Column({length: 50})
+  family: string;
+
+  @Column({length: 120, unique: true})
+  email: string;
+
+  @CreateDateColumn()
+  created: Date;
+
+  @UpdateDateColumn()
+  updated: Date;
+
+  @OneToOne(type => User, user => user.profile)
+  user: User;
+}
