0aa0d3c142e9988c8f4a46a2106bd469300521df
majid mohamadi <m.shutdown57@gmail.com>
2019-02-17 20:34:56 +0330

    Cleanup

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,32 +1,34 @@
-import { ValidationPipe } from './../shared/validation.pip';
-import { Controller, Get,
-         Param, Post,
-         Body, Delete, UsePipes, Put } from '@nestjs/common';
+import {
+  Controller, Get,
+  Param, Post,
+  Body, Delete, UsePipes, Put
+} from '@nestjs/common';
 
 import { PostService } from './post.service';
 import { PostDTO } from './dto/post.dto';
+import { ValidationPipe } from './../shared/validation.pip';
 
 @Controller('api/post')
 export class PostController {
-  constructor(private readonly postService: PostService) {}
+  constructor(private readonly postService: PostService) { }
 
   @Get()
   async index() {
-      const posts = await this.postService.index();
-      return posts;
+    const posts = await this.postService.index();
+    return posts;
   }
 
   @Get(':postID')
   async show(@Param('postID') postID) {
-      const post = await this.postService.show(postID);
-      return post;
+    const post = await this.postService.show(postID);
+    return post;
   }
 
   @Post()
   @UsePipes(new ValidationPipe())
   async create(@Body() data: PostDTO) {
-      const post = await this.postService.create(data);
-      return post;
+    const post = await this.postService.create(data);
+    return post;
   }
 
   @Put(':id')
@@ -37,7 +39,7 @@ export class PostController {
 
   @Delete(':postID')
   async delete(@Param('postID') postID) {
-      const posts = await this.postService.delete(postID);
-      return posts;
+    const posts = await this.postService.delete(postID);
+    return posts;
   }
 }
