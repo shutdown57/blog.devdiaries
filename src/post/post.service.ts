7adfc1a9f380f155302ef63128557b8a5141e366
majid mohamadi <m.shutdown57@gmail.com>
2019-02-17 20:36:38 +0330

    Cleanup

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,9 +1,9 @@
-import { PostDTO, PostRO } from './dto/post.dto';
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { Post } from './post.entity';
+import { PostDTO, PostRO } from './dto/post.dto';
 
 @Injectable()
 export class PostService {
@@ -11,14 +11,14 @@ export class PostService {
   constructor(
     @InjectRepository(Post)
     private readonly postRepository: Repository<Post>,
-  ) {}
+  ) { }
 
   async index(): Promise<PostRO[]> {
     return await this.postRepository.find();
   }
 
   async show(postID: string): Promise<PostRO> {
-    const post = await this.postRepository.findOne({id: postID});
+    const post = await this.postRepository.findOne({ id: postID });
     if (!post) {
       throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
     }
@@ -36,17 +36,17 @@ export class PostService {
   }
 
   async update(id: string, data: Partial<PostDTO>): Promise<PostRO> {
-    let post = await this.postRepository.findOne({where: {id}});
+    let post = await this.postRepository.findOne({ where: { id } });
     if (!post) {
       throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
     }
-    await this.postRepository.update({id}, data);
-    post = await this.postRepository.findOne({where: {id}});
+    await this.postRepository.update({ id }, data);
+    post = await this.postRepository.findOne({ where: { id } });
     return post;
   }
 
   async delete(id: string): Promise<PostRO> {
-    const post = await this.postRepository.findOne({where: {id}});
+    const post = await this.postRepository.findOne({ where: { id } });
     if (!post) {
       throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
     }
