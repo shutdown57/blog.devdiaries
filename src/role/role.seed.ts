c8afe336584a49b641546e7c8703d38a6aad60c3
majid mohamadi <m.shutdown57@gmail.com>
2019-03-08 04:31:25 +0330

    Added role seed

---
+++ src/role/role.seed.ts
@@ -0,0 +1,8 @@
+export const userRoles = [
+  { name: 'guest', code: 0 },
+  { name: 'client', code: 1 },
+  { name: 'dr', code: 2 },
+  { name: 'couch', code: 4 },
+  { name: 'mod', code: 8 },
+  { name: 'admin', code: 64 }
+];
