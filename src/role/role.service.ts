3e53ce48337dc50350e843ce3a652c79116604fa
majid mohamadi <m.shutdown57@gmail.com>
2019-03-08 04:07:04 +0330

    Added http exception , http status, orm repository
    added role entity, role response object, role dto
    added user role seed
    added index role method
    added index user method
    added show method
    added create method
    added seed method

--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Role } from './role.entity';
+import { RoleRO, RoleDTO } from './dto/role.dto';
+import { userRoles } from './role.seed';
 
 @Injectable()
-export class RoleService {}
+export class RoleService {
+  constructor(
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>,
+  ) { }
+
+  async indexRole(): Promise<RoleRO[]> {
+    const roles = await this.roleRepository.find();
+    if (!roles) {
+      throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+    }
+    return roles;
+  }
+
+  async indexUser() { }
+
+  async show(name: string): Promise<RoleRO> {
+    const role = await this.roleRepository.findOne({ where: { name } });
+    if (!role) {
+      throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+    }
+    return role;
+  }
+
+  async create(data: RoleDTO): Promise<RoleRO> {
+    let role = await this.roleRepository.findOne({ where: { name: data.name } });
+    if (role) {
+      throw new HttpException('Conflict', HttpStatus.CONFLICT);
+    }
+    role = await this.roleRepository.create(data);
+    await this.roleRepository.save(role);
+    return role;
+  }
+
+  async seed() {
+    for (let i = 0; i < userRoles.length; i++) {
+      if (this.roleRepository.findOne({ where: { name: userRoles[i].name } })) {
+        continue;
+      }
+      let role = await this.roleRepository.create(userRoles[i]);
+      await this.roleRepository.save(role);
+    }
+  }
+}
