8559b01a383aace43b067d2e0c3ec408adff1ed4
majid mohamadi <m.shutdown57@gmail.com>
2019-03-08 03:52:41 +0330

    Added logging, http filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,29 @@
-import { LoggingInterceptor } from './shared/logging.interceptor';
-import { HttpErrorFilter } from './shared/http-error.filter';
 import { Module } from '@nestjs/common';
 import { Connection } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-// import { PostModule } from './post/post.module';
-import { UserModule } from './user/user.module';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
+import { LoggingInterceptor } from './shared/logging.interceptor';
+import { HttpErrorFilter } from './shared/http-error.filter';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
+import { RoleModule } from './role/role.module';
+
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    UserModule,
+    PostModule
+  ],
   controllers: [AppController],
-  providers: [AppService, {provide: APP_FILTER, useClass: HttpErrorFilter}, {provide: APP_INTERCEPTOR, useClass: LoggingInterceptor}],
+  providers: [
+    AppService,
+    { provide: APP_FILTER, useClass: HttpErrorFilter },
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor }
+  ],
 })
 export class AppModule {
-  constructor(private readonly connection: Connection) {}
+  constructor(private readonly connection: Connection) { }
 }
